dba25d316f75b978e928309003be570caa8f7d10
Aayush9029 <aayushpokharel9029@gmail.com>
2026-02-24 20:44:24 -0500

    better

--- GloamKit/Sources/UI/FloatingCapsule/FloatingCapsuleStyles.swift
+++ GloamKit/Sources/UI/FloatingCapsule/FloatingCapsuleStyles.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 enum CapsuleStyle {
     static let hStackSpacing: CGFloat = 8
-    static let minWidth: CGFloat = 120
-    static let maxWidth: CGFloat = 280
+    static let minWidth: CGFloat = 200
+    static let maxWidth: CGFloat = 320
     static let height: CGFloat = 52
 }
